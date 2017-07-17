6c70f052b1760444b2772fd1b98ac02094ba52aa
Chris Pisano <cpisano86@gmail.com>
2017-07-17 11:26:48 -0400

    modified code

--- lib/captain/test.rb
+++ lib/captain/test.rb
@@ -6,21 +6,21 @@ module Captain
       @test = test
     end
 
-    def gradle(cmd)
-       system("gradle #{cmd}")
+    def gradle(*cmd)
+       system('gradle', '--daemon', cmd)
     end
 
-    def maven(cmd)
-      system("mvn #{cmd}")
+    def maven(*cmd)
+      system('mvn',cmd)
     end
 
-    def rubocop(cmd)
-      system("rubocop #{cmd}")
+    def rubocop(*cmd)
+      system('rubocop', cmd)
     end
 
     def cft(profile, file)
-      system("aws --profile #{profile} cloudformation validate-template \
-             --template-body file://#{file}")
+      system('aws', '--profile', profile, 'cloudformation', \
+             'validate-template', '--template-body', "file://#{file}")
     end
   end
 end
