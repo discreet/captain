3958d022180b962e4053ad5e4ca56070c69f68d9
Chris Pisano <cpisano86@gmail.com>
2017-07-18 15:19:51 -0400

    removed prepare-commit-msg hooks since they are minimally useful

--- lib/captain/signature.rb
+++
@@ -1,9 +0,0 @@
-module Captain
-  class Signature
-
-    def self.signature
-      shuffled_string = 'Captain'.split('').shuffle.join
-      hashID = Digest::SHA1.hexdigest shuffled_string
-    end
-  end
-end
