ae8d2f47be990c73878199b8a9135b671b084425
Chris Pisano <cpisano86@gmail.com>
2017-07-21 11:34:05 -0400

    changed parameters passed to class

--- lib/captain/rules.rb
+++ lib/captain/rules.rb
@@ -1,23 +1,27 @@
 module Captain
   class Rules
-    real_line_number = line_number + 1
 
-    def self.title_line(line, line_number)
-      if line_number.zero? && line.length > 50
-        return "Error #{real_line_number}: First line should be less than 50 \
+    def initialize(line_number)
+      @line_number = line_number
+      @real_line_number = line_number + 1
+    end
+
+    def self.title_line(line)
+      if @line_number.zero? && line.length > 50
+        return "Error #{@real_line_number}: First line should be less than 50 \
         characters in length."
       end
     end
 
-    def self.blank_line(line, line_number)
-      if line_number == 1 && !line.empty?
-        return "Error #{real_line_number}: Second line should be empty."
+    def self.blank_line(line)
+      if @line_number == 1 && !line.empty?
+        return "Error #{@real_line_number}: Second line should be empty."
       end
     end
 
     def self.line_chars(line)
       if line.length > 72 && line[0, 1] != '#'
-        return "Error #{real_line_number}: No line should be over 72 \
+        return "Error #{@real_line_number}: No line should be over 72 \
         characters long."
       end
     end
