b0471f38fc7c1382cffa79329abcdbc9c11d3580
Chris Pisano <cpisano86@gmail.com>
2017-07-24 11:06:59 -0400

    version tag

---
+++ lib/captain/version.rb
@@ -0,0 +1,18 @@
+module Captain
+  class Version
+
+    def self.get
+      Dir.chdir(File.expand_path(File.dirname(__FILE__)))
+      Dir.chdir('../../')
+
+      @properties = File.read('pipeline.properties')
+      increment = @properties[/major$|minor$|patch$/]
+    end
+
+    def self.change(version)
+      new_increment = @properties.gsub(/(major$|minor$|patch$)/, version)
+
+      File.open('pipeline.properties', 'w') { |file| file.puts new_increment }
+    end
+  end
+end
