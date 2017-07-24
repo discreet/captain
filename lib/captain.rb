1b930018654143d3bb1aba9393b5ec871008951c
Chris Pisano <cpisano86@gmail.com>
2017-07-24 09:50:40 -0400

    removed deleted class

--- lib/captain.rb
+++ lib/captain.rb
@@ -2,7 +2,6 @@ require_relative 'captain/logger'
 require_relative 'captain/changes'
 require_relative 'captain/test'
 require_relative 'captain/rules'
-require_relative 'captain/directory'
 
 module Captain
 end
