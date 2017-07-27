1f14e382b769caa22183ea324906a18f9c6598de
Chris Pisano <cpisano86@gmail.com>
2017-07-27 15:29:34 -0400

    refactoring
    fixing commit message validation

--- lib/captain/rules.rb
+++ lib/captain/rules.rb
@@ -1,9 +1,10 @@
 module Captain
   class Rules
     attr_reader :errors
+    attr_reader :lines
 
     def initialize(lines)
-      @lines = lines
+      @lines = lines.reject { |line| line =~ /\A\s*#/ }
       @errors = []
     end
 
@@ -12,31 +13,33 @@ module Captain
     end
 
     def valid_title?
-      return true if title.length <= 50
+      return true if title.length <= 50 && !title.start_with?("\n")
       @errors << 'Error Title Line: Title should be less than 50 characters'
+      return false
     end
 
-    def blank_line
-      @blank_line ||= @lines[1]
+    def second_line
+      @second_line ||= @lines[1]
     end
 
-    def valid_blank_line?
-      return true if blank_line.empty?
+    def valid_second_line?
+      return true if second_line.start_with?("\n")
       @errors << 'Error Blank Line: Second line should be empty'
+      return false
     end
 
     def body
-      @body ||= @lines[2..-1].reject { |line| line =~ /\A\s*#/ }
+      @body ||= @lines[2..-1]
     end
 
     def valid_body?
-      return true if body.all? { |line| line.length <= 72 }
+      return true if body.all? { |line| line.length <= 72 && line.length > 0 }
       @errors << 'Error Line Length: No line should be over 72 characters'
+      return false
     end
 
     def validate!
-      valid_title? && valid_blank_line? && valid_body?
-      return false if @errors
+      valid_title? && valid_second_line? && valid_body?
     end
   end
 end
