c9e7d2c2fe7164381590d835062dfbc992fa96f3
Chris Pisano <cpisano86@gmail.com>
2017-07-27 16:42:56 -0400

    refactor
    rubocop errors

--- lib/captain/rules.rb
+++ lib/captain/rules.rb
@@ -8,34 +8,38 @@ module Captain
       @errors = []
     end
 
+    def filtered_lines
+      @filtered_lines ||= @lines.reject { |line| line =~ /\A\s*#/ }
+    end
+
     def title
-      @title ||= @lines.first
+      @title ||= filtered_lines.first
     end
 
     def valid_title?
       return true if title.length <= 50
       @errors << 'Error Title Line: Title should be less than 50 characters'
-      return false
+      false
     end
 
     def second_line
-      @second_line ||= @lines[1]
+      @second_line ||= filtered_lines[1]
     end
 
     def valid_second_line?
       return true if second_line.start_with?("\n")
       @errors << 'Error Blank Line: Second line should be empty'
-      return false
+      false
     end
 
     def body
-      @body ||= @lines[2..-1].reject { |line| line =~ /\A\s*#/ }
+      @body ||= filtered_lines[2..-1]
     end
 
     def valid_body?
       return true if body.all? { |line| line.length <= 72 }
       @errors << 'Error Line Length: No line should be over 72 characters'
-      return false
+      false
     end
 
     def validate!
