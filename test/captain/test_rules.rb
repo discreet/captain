9b3081a1b6708da662e9f46d5e1b823f06ceacd3
Chris Pisano <cpisano86@gmail.com>
2017-08-01 15:39:48 -0400

    rubocop fixes

--- test/captain/test_rules.rb
+++ test/captain/test_rules.rb
@@ -3,7 +3,7 @@ require_relative '../../lib/captain/rules'
 
 class TestRules < Minitest::Test
   def test_validate_rules
-    rules = Captain::Rules.new(['foo', "\n", 'baz'])
+    rules = Captain::Rules.new(%w[title \n body])
     assert rules.validate! == true
   end
 end
